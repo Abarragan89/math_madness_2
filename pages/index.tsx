c5041e8652c471bb4a7dd35ffbdf531de0929c85
Anthony Barragan <anthony.bar.89@gmail.com>
2022-09-18 22:30:10 -0700

    TEST: testing if sounds work on heroku

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/homePage/index.module.css';
+import useSound from 'use-sound';
 
 
 function HomePage() {
+
+    const [play] = useSound('/sounds/buttonClick.wav', {
+        volume: .3
+    })
 
     return (
         <main className={styles.homepageMain}>
@@ -11,12 +16,13 @@ function HomePage() {
             <Image className={styles.homePageImage} src="/rocketShip.png" width="350px" height="250px" alt="spaceship blasting off into space"></Image> <br />
             <Link href='/chooseGame' >
                 <button className={`mainButton ${styles.homePageBtn}`}
-                onClick={() => stop()}
+                onClick={() => play()}
                 ><span>New Adventure</span></button>
                 </Link><br />
             <Link href='/continueGame'>
                 <button
                     className={`mainButton ${styles.homePageBtn}`}
+                    onClick={() => play()}
                 ><span>Continue</span></button>
             </Link>
         </main>
