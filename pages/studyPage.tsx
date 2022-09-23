a286cf5857b48cab5d031f66463f916ca1c51138
Anthony Barragan <anthony.bar.89@gmail.com>
2022-09-22 22:35:34 -0700

    Feature: green aliens are now class based. Multiple show and move across the canvas

--- pages/studyPage.tsx
+++ pages/studyPage.tsx
@@ -1,38 +1,146 @@
-import { useEffect, useRef } from 'react';
-import frameRenderer from '../assets/frameRender';
-
+import { useEffect, useRef, useLayoutEffect } from 'react';
 import styles from '../styles/studyPage/studyPage.module.css';
+import Alien  from '../assets/alienClass';
+
+
 
 function StudyPage() {
-    const canvasRef = useRef(null)
+    const size = { width: 370, height: 500 };
 
-    useEffect(() => {
+    const canvasRef = useRef(null);
+    // reference to the animation reference to stop animation
+    const requestIdRef = useRef(null);
 
-        const size = { width: 370, height: 400 };
-        
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-        
-        context.arc(50, 50, 10, 0, Math.PI * 2, false);
-        context.fillStyle = 'green';
-        context.fill();        
-
-        context.arc(100,100, 20, 0, Math.PI * 2, false);
-        context.fillStyle = 'red';
-        context.fill();
+    const alien1 = useRef(null);
+    const alien2 = useRef(null);
+    const alien3 = useRef(null);
+    const alien4 = useRef(null);
 
 
 
+    // // Set up canvas
+    // const canvas = canvasRef.current
+    // const context = canvas.getContext('2d')
+    // // Set up image
+    // const spaceship = new Image();
+    // spaceship.src = '/rocketShip.png';
+    // // Put properties of image in object
+    // const spaceshipPos = {
+    //     x: canvas.width / 2,
+    //     y: 450,
+    //     width: 170,
+    //     height: 100
+    // }
+    // // animate function for image
+    // function animate() {
+    //     context.clearRect(0, 250, canvas.width, canvas.height);  // clear canvas
+    //     context.drawImage(spaceship, spaceshipPos.x, spaceshipPos.y, 170, 100);   // draw image at current position
+    //     spaceshipPos.x -= 4
+    //     if (spaceshipPos.x > 250) requestAnimationFrame(animate)        // loop
+    //   }
+    // spaceship.onload = animate
 
-        // const image = new Image();
-        // image.src = "/rocketShip.png";
-        // image.onload = () => {
-        //     context.drawImage(image, 50, 50, 250, 150)
-        // }
-        frameRenderer.call(context, size);
+    // const updateBall = () => {
+    //     const ball = alien1Ref.current;
+    //     ball.x += ball.vx;
+    //     ball.y += ball.vy;
+    //     if (ball.x + ball.radius >= size.width) {
+    //         ball.vx = -ball.vx;
+    //         ball.x = size.width - ball.radius;
+    //     }
+    //     if (ball.x - ball.radius <= 0) {
+    //         ball.vx = -ball.vx;
+    //         ball.x = ball.radius;
+    //     }
+    //     if (ball.y + ball.radius >= size.height / 2) {
+    //         ball.vy = -ball.vy;
+    //         ball.y = size.height / 2 - ball.radius;
+    //     }
+    //     if (ball.y - ball.radius <= 0) {
+    //         ball.vy = -ball.vy;
+    //         ball.y = ball.radius;
+    //     }
+    // };
 
-    }, [])
+    // class Alien {
+    //     x: number;
+    //     y: number;
+    //     r: number;
+    //     ctx
+    //     answer: string;
+    //     velX:number;
+    //     velY:number;
+    //     constructor
+    //     (ctx, x: number, y: number, r: number, answer: string, velX:number, velY:number) {
+    //         this.x = x,
+    //         this.y = y,
+    //         this.r = r,
+    //         this.answer = answer;
+    //         this.ctx = ctx;
+    //         this.velX = velX;
+    //         this.velY = velY;
+    //     }
+    //     drawAlien() {
+    //         this.ctx.save();
+    //         this.ctx.beginPath();
+    //         this.ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    //         this.ctx.fillStyle = 'green';
+    //         this.ctx.fill();
+    //         this.ctx.closePath();
+    //         this.ctx.restore();
+    //     }
+    //     moveAlien() {
+    //         this.drawAlien();
+    //         this.x += this.velX;
+    //         // this.y += this.velY;
+    //         if (this.x + this.r >= this.ctx.width) {
+    //             this.velX = -this.velX;
+    //             this.x = this.ctx.width - this.r;
+    //         }
+    //         if (this.x - this.r <= 0) {
+    //             this.velX = -this.velX;
+    //             this.x = this.r;
+    //         }
+    //         if (this.y + this.r >= this.ctx.height / 2) {
+    //             this.velY = -this.velY;
+    //             this.y = this.ctx.height / 2 - this.r;
+    //         }
+    //         if (this.y - this.r <= 0) {
+    //             this.velY = -this.velY;
+    //             this.y = this.r;
+    //         }
+    //     }
+    // }
 
+
+    const renderFrame = () => {
+        alien1.current.moveAlien();
+        alien2.current.moveAlien();
+        alien3.current.moveAlien();
+        alien4.current.moveAlien();
+        console.log(alien1)
+    };
+
+    const tick = () => {
+        if (!canvasRef.current) return;
+        canvasRef.current.getContext('2d').clearRect(0, 0, size.width, size.height / 2);
+
+        renderFrame();
+        requestIdRef.current = requestAnimationFrame(tick);
+    };
+
+    useLayoutEffect(() => {
+        const ctx = canvasRef.current.getContext('2d');
+        alien1.current = new Alien(ctx, 300, 20, 15, '56', 1, 1);
+        alien2.current = new Alien(ctx, 120, 60, 15, '56', 1, 1);
+        alien3.current = new Alien(ctx, 0, 100, 15, '56', 1, 1);
+        alien4.current = new Alien(ctx, 200, 140, 15, '56', 1, 1);
+
+        requestIdRef.current = requestAnimationFrame(tick);
+        return () => {
+            cancelAnimationFrame(requestIdRef.current);
+        };
+    }, []);
 
 
     return (
@@ -46,7 +154,8 @@ function StudyPage() {
                 </div>
             </div>
             <p><span>num1</span> x <span>num2</span></p>
-            <canvas ref={canvasRef} />
+            <canvas width={370} height={500} ref={canvasRef} />
+
         </main>
     )
 }
